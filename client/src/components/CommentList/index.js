0e331cee84bd440826015f34a34372aa5caef433
AE SChan <abigailsinchan@gmail.com>
2023-08-16 22:01:43 -0400

    added styling back to singles Recipe

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -1,31 +1,38 @@
 import React from 'react';
 
-
-const CommentList = ({ comments = [], handleRemoveComment, authService }) => {
+const CommentList = ({ comments = [],
+  handleRemoveComment, authService 
+ }) => {
   if (!comments.length) {
     return <h3>No Comments Yet</h3>;
   }
 
   return (
     <>
-      <h3 className="p-5 display-inline-block" style={{ borderBottom: '1px dotted #1a1a1a' }}>
+      <h3
+        className="p-5 display-inline-block"
+        style={{ borderBottom: '1px dotted #1a1a1a' }}
+      >
         Comments
       </h3>
       <div className="flex-row my-4">
-        {comments.map((comment) => (
-          <div key={comment._id} className="col-12 mb-3 pb-3">
-            <div className="p-3 bg-dark text-light">
+        {comments &&
+          comments.map((comment) => (
+            <div key={comment._id} className="col-12 mb-3 pb-3">
+              <div className="p-3 bg-dark text-light">
               <h5 className="card-header">
-                {comment.commentAuthor ? (
-                  <>
-                    {comment.commentAuthor.username} commented{' '}
-                    <span style={{ fontSize: '0.825rem' }}>on {comment.createdAt}</span>
-                  </>
-                ) : (
-                  'Unknown commented'
-                )}
-              </h5>
-              <p className="card-body">{comment.commentText}</p>
+                  {comment.commentAuthor ? (
+                    <>
+                      {comment.commentAuthor.username} commented{' '}
+                      <span style={{ fontSize: '0.825rem' }}>
+                        on {comment.createdAt}
+                      </span>
+                    </>
+                  ) : (
+                    'Unknown commented'
+                  )}
+                </h5>
+                <p className="card-body">{comment.commentText}</p>
               {comment.commentAuthor._id === authService.getUserId() && (
                 <button
                   className="btn btn-danger"
--- client/src/pages/SingleRecipe.js
+++ client/src/pages/SingleRecipe.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 
@@ -6,20 +6,32 @@ import CommentList from '../components/CommentList';
 import CommentForm from '../components/CommentForm';
 
 import AuthService from '../utils/auth';
-import { QUERY_RECIPE, SAVE_RECIPE } from '../utils/queries';
+import { QUERY_RECIPE, SAVE_RECIPE, QUERY_SAVED_RECIPES } from '../utils/queries';
 import { REMOVE_COMMENT } from '../utils/mutations';
 
 const SingleRecipe = () => {
   const { recipeId } = useParams();
-
   const { loading, data } = useQuery(QUERY_RECIPE, {
     variables: { recipeId: recipeId },
   });
 
   const recipe = data?.recipe;
 
+  const [isRecipeSaved, setIsRecipeSaved] = useState(
+    localStorage.getItem(`savedRecipe_${recipeId}`) === 'true'
+  );
+  
+
   const [saveRecipe] = useMutation(SAVE_RECIPE, {
     variables: { recipeId: recipe?._id },
+    onCompleted: () => {
+      setIsRecipeSaved(true);
+      localStorage.setItem(`savedRecipe_${recipeId}`, 'true');
+    },
+    onError: (error) => {
+      console.error("Error saving recipe:", error);
+    },
+    refetchQueries: [{ query: QUERY_SAVED_RECIPES, variables: { userId: AuthService.getProfile().id } }],
   });
 
   const [removeComment] = useMutation(REMOVE_COMMENT, {
@@ -59,35 +71,55 @@ const SingleRecipe = () => {
     return <div>Loading...</div>;
   }
 
-
-
   return (
     <div className="my-3">
       {/* Render the recipe data */}
-      <h2>{recipe.title}</h2>
+      <h2 className="card-header bg-dark text-light p-2 m-0">
+        {recipe.title}<br />
+        <span style={{ fontSize: '1rem' }}>
+        Created by {recipe.createdBy.username} on {recipe.createdAt}
+        </span>
+      </h2>
+      <div className="bg-light py-4">
+        <blockquote
+          className="p-4"
+          style={{
+            fontSize: '1.5rem',
+            fontStyle: 'italic',
+            border: '2px dotted #1a1a1a',
+            lineHeight: '1.5',
+          }}
+        >
+         <h5>{recipe.title}</h5> 
       <p>Ingredients: {recipe.ingredients.join(', ')}</p>
       <p>Instructions: {recipe.instructions}</p>
-      <p>Created by: {recipe.createdBy.username}</p>
-            <button
+      
+      <button
         className="btn btn-primary"
         onClick={() => {
-          saveRecipe();
+          if (isRecipeSaved) {
+            localStorage.removeItem(`savedRecipe_${recipeId}`);
+          } else {
+            saveRecipe();
+          }
         }}
+        disabled={isRecipeSaved} // Disable the button if the recipe is already saved
       >
-        Save Recipe
+        {isRecipeSaved ? 'Recipe Saved' : 'Save Recipe'}
       </button>
-      {/* ... Other content ... */}
+      </blockquote>
+      </div>
       <div className="my-5">
-      <CommentList 
-      comments={recipe.comments}
-      handleRemoveComment={handleRemoveComment}
-      authService={AuthService} // Pass the AuthService instance
-      />
+        <CommentList
+         comments={recipe.comments}
+         handleRemoveComment={handleRemoveComment}
+         authService={AuthService} // Pass the AuthService instance
+         />
       </div>
       <div className="m-3 p-4" style={{ border: '1px dotted #1a1a1a' }}>
         <CommentForm recipeId={recipe._id} />
       </div>
-
+     
     </div>
   );
 };
